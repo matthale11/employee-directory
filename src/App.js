f9bb5be8836a700d6c05d5c9a84bff81380da4b3
Matt Hale <mhale@factharbor.com>
2021-05-13 10:06:28 -0400

    added sort feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,65 @@
 import React, { Component } from "react";
 import "./App.css";
-import UserTable from "./components/UserTable";
+import Table from "react-bootstrap/Table";
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      displayedUsers: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://randomuser.me/api/?results=50")
+      .then((res) => res.json())
+      .then((results) => {
+        this.setState({
+          users: results.results,
+          displayedUsers: results.results,
+        });
+      });
+  }
+
+  sortUsers = (categorya, categoryb) => {
+    const displayedUsers = this.state.users.sort((a, b) =>
+      a[categorya][categoryb] > b[categorya][categoryb] ? 1 : -1
+    );
+    this.setState({ displayedUsers });
+  };
+
+    // TODO: Create search box and filter feature.
+
   render() {
     return (
       <div className="App">
-        <UserTable />
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th onClick={() => this.sortUsers("name", "first")}>First (sort)</th>
+              <th onClick={() => this.sortUsers("name", "last")}>Last (sort)</th>
+              <th onClick={() => this.sortUsers("location", "country")}>
+                Country (sort)
+              </th>
+              <th>Username</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.displayedUsers.map((user) => {
+              return (
+                <tr>
+                  <td>{user.name.first}</td>
+                  <td>{user.name.last}</td>
+                  <td>{user.location.country}</td>
+                  <td>{user.login.username}</td>
+                  <td>{user.email}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
       </div>
     );
   }
